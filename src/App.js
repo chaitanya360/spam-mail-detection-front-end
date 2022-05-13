631ddc4be52efbb59884112990bee47d88c8bfdd
Chaitanya360 <chaitanyajadhav360@gmail.com>
2022-05-13 13:22:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [prediction, setPrediction] = useState(false);
+
+  const fetchData = () => {
+    fetch("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>File name</label>
+      <input />
+
+      <button>Predict</button>
+      <div className="output">output goes here</div>
     </div>
   );
 }
